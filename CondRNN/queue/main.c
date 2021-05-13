1c249f8fcd5fe94cb1096b7c2bf7165defb6881c
Shuffle-Official <D__OH@outlook.com>
2021-05-14 06:13:17 +0800

    finalize queue

--- CondRNN/queue/main.c
+++ CondRNN/queue/main.c
@@ -1,47 +1,65 @@
 #include <stdio.h>
 #define MAX_N_SERVER 1000
-#define MAX_N_PERIOD 100
 
-void single_server_queue(float wait_time[], int n_customer);
-void multi_server_queue(float wait_time[], int n_server[], float duration[],
-                        int n_customer, int n_period);
-int max_of_non_negative_list(int list[], int length);
-void get_server_avail_till(float duration_ls[], int n_server_ls[], float *avail_till_time_ls);
-float* cumsum(float list[], int length);
-
-
-float arrival_time_ls[] = {15, 47, 71, 111, 123, 152, 166, 226, 310, 320};
-float service_time_ls[] = {43, 36, 34, 30, 38, 40, 31, 29, 36, 30};
-int n_customer = sizeof (arrival_time_ls) / sizeof (arrival_time_ls[0]);
-
-int n_server_ls[] = {3,1,2};
-float duration_ls[] = {100, 50, 200};
-int n_period = 3;
+void single_server_queue(float arrival_time_ls[], float service_time_ls[], float wait_time_ls[], int n_customer);
+void const_multi_server_queue(float arrival_time_ls[], float service_time_ls[], float wait_time_ls[],
+                              int n_server, int n_customer);
+void changing_multi_server_queue(float arrival_time_ls[], float service_time_ls[], float wait_time_ls[],
+                                 int n_server_ls[], float duration_ls[], int n_customer, int n_period);
+int allocate_server(float busy_till_time_ls[], int n_server_ls[], int n_period,
+                    float cum_duration_ls[], int max_n_server);
 
 int main() {
+    // for single server queue
+    float arrival_time_ls_1[] = {15, 47, 71, 111, 123, 152, 166, 226, 310, 320};
+    float service_time_ls_1[] = {43, 36, 34, 30, 38, 40, 31, 29, 36, 30};
+    int n_customer = sizeof (arrival_time_ls_1) / sizeof (arrival_time_ls_1[0]);
+
+
+    // for multi server queue with constant number of servers
+    float arrival_time_ls_2[] = {15, 47, 71, 111, 123, 152, 166, 226, 310, 320};
+    float service_time_ls_2[10] = {};
+    for (int i = 0; i < n_customer; ++i){
+        service_time_ls_2[i] = service_time_ls_1[i] * 5;
+    }
+    int n_server = 4;
+
+    // for multi server queue with changing number of servers
+    float arrival_time_ls_3[] = {15, 47, 71, 111, 123, 152, 166, 226, 310, 320};
+    float service_time_ls_3[10] = {};
+    for (int i = 0; i < n_customer; ++i){
+        service_time_ls_3[i] = service_time_ls_1[i] * 5;
+    }
+    int n_server_ls[] = {4,4,4,4};
+    float duration_ls[] = {50, 50, 100, 50};
+    int n_period = 4;
+
     /* single server queue */
-    float single_wait_time_ls[n_customer];
-    single_server_queue(single_wait_time_ls, n_customer);
+    float wait_time_ls_1[n_customer];
+    single_server_queue(arrival_time_ls_1, service_time_ls_1, wait_time_ls_1, n_customer);
     printf("results for single server queue\n");
-    for (int i = 0; i < n_customer; i = i + 1)printf("%f\n", single_wait_time_ls[i]);
+    for (int i = 0; i < n_customer; i = i + 1)printf("%f\n", wait_time_ls_1[i]);
     printf("**********\n");
 
-    /* multi server queue */
-    float multi_wait_time_ls[n_customer];
+    /* constant multi server queue */
+    float wait_time_ls_2[n_customer];
+    const_multi_server_queue(arrival_time_ls_2, service_time_ls_2, wait_time_ls_2, n_server, n_customer);
+    printf("results for constant multi server queue\n");
+    for (int i = 0; i < n_customer; i = i + 1)printf("%f\n", wait_time_ls_2[i]);
+    printf("**********\n");
 
-    float avail_till_time_array[MAX_N_SERVER][MAX_N_PERIOD];
-    get_server_avail_till(duration_ls, n_server_ls, avail_till_time_array);
-
-//    multi_server_queue(multi_wait_time_ls, n_server_ls, duration_ls, n_customer,
-//                       n_period);
-//    printf("results for multi server queue\n");
-//    for (int i = 0; i < n_customer; i = i + 1)printf("%f\n", multi_wait_time_ls[i]);
-//    printf("**********\n");
+    /* changing multi server queue */
+    float wait_time_ls_3[n_customer];
+    changing_multi_server_queue(arrival_time_ls_3, service_time_ls_3, wait_time_ls_3, n_server_ls, duration_ls,
+                                n_customer, n_period);
+    printf("results for changing multi server queue\n");
+    for (int i = 0; i < n_customer; i = i + 1)printf("%f\n", wait_time_ls_3[i]);
+    printf("**********\n");
     return 0;
 }
 
 
-void single_server_queue(float wait_time_ls[], int n_customer) {
+void single_server_queue(float arrival_time_ls[], float service_time_ls[], float wait_time_ls[], int n_customer) {
     /* Reference: http://www.cs.wm.edu/~esmirni/Teaching/cs526/section1.2.pdf */
     float c_i_minus_1 = 0;
     float d_i;
@@ -57,93 +75,156 @@ void single_server_queue(float wait_time_ls[], int n_customer) {
     }
 }
 
-
-float* cumsum(float list[], int length){
-    float cumsum_ls[length];
-    float sum = 0;
-    for(int i = 0; i < length; ++i){
-        sum += list[i];
-        cumsum_ls[i] = sum;
-    }
-    return cumsum_ls;
-}
-
-void get_server_avail_till(float duration_ls[], int n_server_ls[], float *avail_till_time_array){
-    float* cumsum_duration_ls;
-    cumsum_duration_ls = cumsum(duration_ls, n_period);
-    int max_n_server = max_of_non_negative_list(n_server_ls, n_period);
-    for(int current_server = 0; current_server < max_n_server; ++current_server){
-
-    }
-}
-
-
-int min_of_non_negative_list(float list[], int length, int *index) {
-    /* find the largest value in a non positive list*/
+int index_min(float list[], int length) {
+    /* find the smallest value in list */
     float min_val_found = list[0];
+    int index = 0;
     for (int i = 0; i < length; i++) {
         if (min_val_found > list[i]) {
             min_val_found = list[i];
-            *index = i;
+            index = i;
         }
     }
-    return min_val_found;
+    return index;
 }
 
-void handle_one_period(float wait_time_ls[], int n_server, float avail_till_time_ls[],
-                       float duration, int n_customer, float busy_till_time_ls[]){
-    /* for each customer,
-     * if there's idle servers:
-     *  assign him to the idle server with smallest id
-     * else:
-     *  assign him to the first server becomes idle
-     * */
-    float current_arrival_time, earliest_idle_time, intend_finish_time;
-    int server_id, customer_id, index, get_service_immediately; // index is used to store the id for the first server becomes idle
-    for (customer_id = 0; (current_arrival_time = arrival_time_ls[customer_id]) < duration; customer_id++){
-        get_service_immediately = 0;
-        for (server_id = 0; server_id < n_server; server_id++) {
-            if(busy_till_time_ls[server_id] < current_arrival_time){
-                intend_finish_time = current_arrival_time + service_time_ls[customer_id];
-                // if this server can finish service this customer before
-                if (intend_finish_time <= duration){
-                    wait_time_ls[customer_id] = 0;
-                    busy_till_time_ls[server_id] += current_arrival_time + service_time_ls[customer_id];
-                    get_service_immediately = 1;
-                    break;
-                }
+
+
+void const_multi_server_queue(float arrival_time_ls[], float service_time_ls[], float wait_time_ls[],
+                              int n_server, int n_customer){
+    float busy_till_time_ls[MAX_N_SERVER] = {};
+    for(int customer_id=0; customer_id < n_customer; ++customer_id){
+        float current_arrival_time = arrival_time_ls[customer_id];
+        // allocate one server
+        int server_id = index_min(busy_till_time_ls, n_server);
+        float earliest_start_time = busy_till_time_ls[server_id];
+        if (earliest_start_time < current_arrival_time){
+            wait_time_ls[customer_id] = 0;
+            busy_till_time_ls[server_id] = current_arrival_time + service_time_ls[customer_id];
+        } else {
+            wait_time_ls[customer_id] = earliest_start_time - current_arrival_time;
+            busy_till_time_ls[server_id] += service_time_ls[customer_id];
+        }
+    }
+}
+
+void cumsum(float list[], int length, float result[]){
+    float cumsum_val = 0;
+    for (int i = 0; i < length; ++i){
+        cumsum_val += list[i];
+        result[i] = cumsum_val;
+    }
+}
+
+int max(int list[], int length){
+    int largest_val = 0;
+    for (int i = 0; i < length; ++i){
+        if (list[i] > largest_val){
+            largest_val = list[i];
+        }
+
+    }
+    return largest_val;
+}
+
+
+void changing_multi_server_queue(float arrival_time_ls[], float service_time_ls[], float wait_time_ls[],
+                              int n_server_ls[], float duration_ls[], int n_customer, int n_period){
+    float busy_till_time_ls[MAX_N_SERVER] = {};
+    float cum_duration_ls[n_period];
+    cumsum(duration_ls, n_period, cum_duration_ls);
+    int max_n_server = max(n_server_ls, n_period);
+    for(int customer_id=0; customer_id < n_customer; ++customer_id){
+        float current_arrival_time = arrival_time_ls[customer_id];
+        // allocate one server
+
+        int server_id = allocate_server(busy_till_time_ls, n_server_ls, n_period, cum_duration_ls,
+                                        max_n_server);
+        if (server_id == -1){
+            wait_time_ls[customer_id] = -1;
+        } else{
+            float earliest_start_time = busy_till_time_ls[server_id];
+            if (earliest_start_time < current_arrival_time){
+                wait_time_ls[customer_id] = 0;
+                busy_till_time_ls[server_id] = current_arrival_time + service_time_ls[customer_id];
+            } else {
+                wait_time_ls[customer_id] = earliest_start_time - current_arrival_time;
+                busy_till_time_ls[server_id] += service_time_ls[customer_id];
             }
         }
-        if (!get_service_immediately){
-            earliest_idle_time = min_of_non_negative_list(busy_till_time_ls, n_server, &index);
-            wait_time_ls[customer_id] = earliest_idle_time - current_arrival_time;
-            busy_till_time_ls[index] += service_time_ls[customer_id];
-        }
     }
 }
 
 
-void multi_server_queue(float wait_time_ls[], int n_server_ls[],
-                        float duration_ls[], int n_customer, int n_period){
-    int max_n_server = max_of_non_negative_list(n_server_ls, n_period);
-    float busy_till_time_ls[MAX_N_SERVER] = {};
-    float avail_till_time_array[MAX_N_SERVER][MAX_N_PERIOD] = {};
-    for ( int period = 0; period < n_period; period = period + 1 ) {
-        float duration = duration_ls[period];
-        int n_server = n_server_ls[period];
+int which_period(float current_time, float cum_duration_ls[], int n_period){
 
-        handle_one_period(wait_time_ls, n_server, avail_till_time_array, duration, n_customer,
-                          busy_till_time_ls);
-    }
-}
-
-int max_of_non_negative_list(int *list, int length){
-    /* find the largest value in a non positive list*/
-    int max_val_found = 0;
-    for(int i = 0; i < length; i++){
-        if(max_val_found < list[i]){
-            max_val_found = list[i];
+    int period_id;
+    for (period_id = 0; period_id < n_period; ++period_id){
+        float period_end_time = cum_duration_ls[period_id];
+        if (period_end_time > current_time){
+            return period_id;
         }
     }
-    return max_val_found;
+    // return period_id = n_period if current_time does not belong to any period
+    return period_id;
+}
+
+int earliest_accessible_period_after(int first_idle_period_id, int server_id, int n_server_ls[], int n_period){
+    int earliest_accessible_period;
+    for (earliest_accessible_period = first_idle_period_id; earliest_accessible_period < n_period; ++earliest_accessible_period){
+        if (n_server_ls[earliest_accessible_period] > server_id){
+            return earliest_accessible_period;
+        }
+    }
+    return earliest_accessible_period;
+}
+
+
+int allocate_server(float busy_till_time_ls[], int n_server_ls[], int n_period,
+                    float cum_duration_ls[], int max_n_server) {
+    float total_duration = cum_duration_ls[n_period - 1];
+    for (int server_id = 0; server_id < max_n_server; server_id++) {
+        // check whether this server is accessible at time busy_till_time_ls[server_id]
+        int first_idle_period_id;
+        int server_accessible;
+        if (busy_till_time_ls[server_id] >= total_duration) {
+            // this server is not accessible forever, thus turn to the next server
+            continue;
+        } else {
+            first_idle_period_id = which_period(busy_till_time_ls[server_id], cum_duration_ls, n_period);
+            int current_n_server;
+            if (first_idle_period_id != n_period) {
+                current_n_server = n_server_ls[first_idle_period_id];
+            } else {
+                // when first_idle_period_id == n_period, the busy_till_time_ls[server_id] is larger than total_duration
+                // thus no server is available
+                current_n_server = 0;
+            }
+            server_accessible = (current_n_server > server_id);
+        }
+
+        // if yes, nothing need to do
+        // if not, find the earliest time that this server becomes accessible,
+        //         change busy_till_time_ls[server_id] to this value
+        if (server_accessible == 0) {
+            int earliest_accessible_period = earliest_accessible_period_after(first_idle_period_id, server_id,
+                                                                            n_server_ls,
+                                                                            n_period);
+
+            if (earliest_accessible_period != n_period) {
+                busy_till_time_ls[server_id] = cum_duration_ls[earliest_accessible_period];
+            } else {
+                // when earliest_accessible_period ==  n_period, this server is not accessible till the end.
+                // set busy_till_time_ls[server_id] to total_duration
+                busy_till_time_ls[server_id] = total_duration;
+            }
+        }
+    }
+    int allocated_server_id = index_min(busy_till_time_ls, max_n_server);
+    int allocated_server_idle_time = busy_till_time_ls[allocated_server_id];
+    if (allocated_server_idle_time >= total_duration) {
+        return -1; // return -1 for no server available anymore
+    } else {
+        return allocated_server_id;
+    }
 }
